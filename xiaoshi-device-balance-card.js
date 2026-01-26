a7fbbe39b82fa3612b45333a1ac5e0722e9df64b
xiaoshi <115949669+xiaoshi930@users.noreply.github.com>
2026-01-26 16:24:26 +0800

    Decrease vibration duration on click event
    Reduce vibration duration from 50ms to 20ms.

--- xiaoshi-device-balance-card.js
+++ xiaoshi-device-balance-card.js
@@ -1008,12 +1008,7 @@ class XiaoshiBalanceCard extends LitElement {
 
   _handleClick(){
     if (navigator.vibrate) {
-      navigator.vibrate(50);
-    }
-    else if (navigator.webkitVibrate) {
-        navigator.webkitVibrate(50); 
-    }
-    else {
+      navigator.vibrate(20);
     }
   }
 
